58bd045395806955a1eb239df4b053211650058b
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-27 05:46:40 -0400

    Validações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
     function handleSubmit(data) {
@@ -11,8 +19,13 @@ export default function SignIn() {
     return (
         <>
             <img src={logo} alt="GoBarber" />
-            <Form onSubmit={handleSubmit}>
-                <Input name="email" type="email" placeholder="Seu e-mail" />
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu e-mail"
+                    autoComplete="off"
+                />
                 <Input
                     name="password"
                     type="password"
