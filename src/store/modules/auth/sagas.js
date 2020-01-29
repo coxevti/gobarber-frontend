75ca59fe030d5720666b106b5b612bb2f8661641
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-29 05:57:59 -0400

    Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -17,6 +17,7 @@ export function* signIn({ payload }) {
             toast.error('Usuário não é prestador');
             return;
         }
+        api.defaults.headers.Authorization = `Bearer ${token}`;
         yield put(signInSuccess(token, user));
         history.push('/dashboard');
     } catch (error) {
@@ -42,7 +43,16 @@ export function* signUp({ payload }) {
     }
 }
 
+export function setToken({ payload }) {
+    if (!payload) return;
+    const { token } = payload.auth;
+    if (token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
 export default all([
+    takeLatest('persist/REHYDRATE', setToken),
     takeLatest('@auth/SIGN_IN_REQUEST', signIn),
     takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
