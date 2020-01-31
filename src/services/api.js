36ce9b30cc45e2642a0647e448d8e3da587563a6
Valdir Coxev <valcox.russo@gmail.com>
2020-01-31 13:15:24 -0400

    Adding axios interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import { store } from '~/store';
+import { signOut } from '~/store/modules/auth/actions';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://localhost:3335',
 });
+
+api.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response.status === 401) {
+            store.dispatch(signOut());
+        } else {
+            return Promise.reject(error);
+        }
+    }
+);
 
 export default api;
